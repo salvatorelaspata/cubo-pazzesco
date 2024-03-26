e0fc715b41c9deedf670241c1789be184c9e690d
SalvatoreLa <salvatore.la.spata@gmail.com>
2024-03-26 21:51:49 +0100

    add gsap.. quasi mi piace

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,3 +1,4 @@
+import gsap from 'gsap'
 import { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
@@ -6,32 +7,65 @@ const MAIN_CUBE = 10
 const MAIN_CUBE_HALF = MAIN_CUBE / 2
 const CUBE_SIZE = 1
 
+const toRandom = (cube: THREE.Mesh, position: THREE.Vector3, prop: 'x' | 'y' | 'z', negative: boolean = false) => {
+    gsap.to(cube.position, {
+        x: prop === 'x' ? !negative ? position.x + Math.random() * 3 : position.x - Math.random() * 3 : position.x,
+        y: prop === 'y' ? !negative ? position.y + Math.random() * 3 : position.y - Math.random() * 3 : position.y,
+        z: prop === 'z' ? !negative ? position.z + Math.random() * 3 : position.z - Math.random() * 3 : position.z,
+        duration: 1,
+        delay: 1,
+        repeatDelay: 1,
+        yoyo: true,
+        repeat: -1,
+        ease: 'power2.inOut',
+    })
+}
+
+const animateScene = (latoDestro: THREE.Mesh[],latoSinistro: THREE.Mesh[],latoSuperiore: THREE.Mesh[],latoInferiore: THREE.Mesh[],latoFrontale: THREE.Mesh[],latoPosteriore: THREE.Mesh[]) => {
+    latoDestro.forEach((cube, i) => {
+        toRandom(cube, cube.position, 'x')
+    })
+    latoSinistro.forEach((cube) => {
+        toRandom(cube, cube.position, 'x', true)
+    })
+    latoSuperiore.forEach((cube) => {
+        toRandom(cube, cube.position, 'y')
+    })
+    latoInferiore.forEach((cube) => {
+        toRandom(cube, cube.position, 'y', true)
+    })
+    latoFrontale.forEach((cube) => {
+        toRandom(cube, cube.position, 'z')
+    })
+    latoPosteriore.forEach((cube) => {
+        toRandom(cube, cube.position, 'z', true)
+    })
+}
 const composeScene = (scene: THREE.Scene) => {
     const group = new THREE.Group()
-    // create main cube
-    const geometryMain = new THREE.BoxGeometry(MAIN_CUBE,MAIN_CUBE,MAIN_CUBE)
-    const materialMain = new THREE.MeshPhysicalMaterial({
-        transparent: true,
-        color: new THREE.Color(0x00ff00),
-    })
-    const mainCube = new THREE.Mesh(geometryMain, materialMain)
-    group.add(mainCube)
+
+    // const geometryMain = new THREE.BoxGeometry(MAIN_CUBE,MAIN_CUBE,MAIN_CUBE)
+    // const materialMain = new THREE.MeshPhysicalMaterial({
+    //     transparent: true,
+    //     color: new THREE.Color(0x00ff00),
+    // })
+    // const mainCube = new THREE.Mesh(geometryMain, materialMain)
+    // group.add(mainCube)
+
     // create cubes around the main cube
     const geometry = new THREE.BoxGeometry(CUBE_SIZE,CUBE_SIZE,CUBE_SIZE)
     const material = new THREE.MeshPhysicalMaterial({
         transmission: 1,
         opacity: 1,
-        metalness: 0.2,
+        metalness: 0.1,
         roughness: 0,
-        ior: 1.5,
+        ior: 1,
         thickness: 0.01,
-        specularIntensity: 1,
-        color: new THREE.Color(0x00ff00),
+        specularIntensity: 0.1,
+        color: new THREE.Color(0xffffff),
         specularColor: new THREE.Color(0xffffff),
         envMapIntensity: 1,
     })
-
-    // posiziona i cubi lungo i bordi del cubo principale
 
     const latoDestro:THREE.Mesh[]       = []
     const latoSinistro:THREE.Mesh[]     = []
@@ -42,9 +76,13 @@ const composeScene = (scene: THREE.Scene) => {
 
     for(let i = 0; i <= MAIN_CUBE; i++) {
         for(let j = 0; j <= MAIN_CUBE; j++) {
+            const random = Math.random()
+            if (random < 0.2) {
+                continue
+            }
             // lato destro
             const cubeDestro = new THREE.Mesh(geometry, material)
-            cubeDestro.position.x = MAIN_CUBE_HALF + Math.random() * 3
+            cubeDestro.position.x = MAIN_CUBE_HALF // + Math.random() * 3
             cubeDestro.position.y = i - MAIN_CUBE_HALF
             cubeDestro.position.z = j - MAIN_CUBE_HALF
             latoDestro.push(cubeDestro)
@@ -52,7 +90,7 @@ const composeScene = (scene: THREE.Scene) => {
 
             // lato sinistro
             const cubeSinistro = new THREE.Mesh(geometry, material)
-            cubeSinistro.position.x = -MAIN_CUBE_HALF - Math.random() * 3
+            cubeSinistro.position.x = -MAIN_CUBE_HALF // - Math.random() * 3
             cubeSinistro.position.y = i - MAIN_CUBE_HALF
             cubeSinistro.position.z = j - MAIN_CUBE_HALF
             latoSinistro.push(cubeSinistro)
@@ -61,7 +99,7 @@ const composeScene = (scene: THREE.Scene) => {
             // lato superiore
             const cubeSuperiore = new THREE.Mesh(geometry, material)
             cubeSuperiore.position.x = i - MAIN_CUBE_HALF
-            cubeSuperiore.position.y = MAIN_CUBE_HALF + Math.random() * 3
+            cubeSuperiore.position.y = MAIN_CUBE_HALF // + Math.random() * 3
             cubeSuperiore.position.z = j - MAIN_CUBE_HALF
             latoSuperiore.push(cubeSuperiore)
             group.add(cubeSuperiore)
@@ -69,7 +107,7 @@ const composeScene = (scene: THREE.Scene) => {
             // lato inferiore
             const cubeInferiore = new THREE.Mesh(geometry, material)
             cubeInferiore.position.x = i - MAIN_CUBE_HALF
-            cubeInferiore.position.y = -MAIN_CUBE_HALF - Math.random() * 3
+            cubeInferiore.position.y = -MAIN_CUBE_HALF // - Math.random() * 3
             cubeInferiore.position.z = j - MAIN_CUBE_HALF
             latoInferiore.push(cubeInferiore)
             group.add(cubeInferiore)
@@ -78,7 +116,7 @@ const composeScene = (scene: THREE.Scene) => {
             const cubeFrontale = new THREE.Mesh(geometry, material)
             cubeFrontale.position.x = i - MAIN_CUBE_HALF
             cubeFrontale.position.y = j - MAIN_CUBE_HALF
-            cubeFrontale.position.z = MAIN_CUBE_HALF + Math.random() * 3
+            cubeFrontale.position.z = MAIN_CUBE_HALF // + Math.random() * 3
             latoFrontale.push(cubeFrontale)
             group.add(cubeFrontale)
 
@@ -86,102 +124,19 @@ const composeScene = (scene: THREE.Scene) => {
             const cubePosteriore = new THREE.Mesh(geometry, material)
             cubePosteriore.position.x = i - MAIN_CUBE_HALF
             cubePosteriore.position.y = j - MAIN_CUBE_HALF
-            cubePosteriore.position.z = -MAIN_CUBE_HALF - Math.random() * 3
+            cubePosteriore.position.z = -MAIN_CUBE_HALF // - Math.random() * 3
             latoPosteriore.push(cubePosteriore)
             group.add(cubePosteriore)
         }
     }
 
-    const initialPositionLatoDestro = latoDestro.map((cube) => cube.position.clone())
-    const initialPositionLatoSinistro = latoSinistro.map((cube) => cube.position.clone())
-    const initialPositionLatoSuperiore = latoSuperiore.map((cube) => cube.position.clone())
-    const initialPositionLatoInferiore = latoInferiore.map((cube) => cube.position.clone())
-    const initialPositionLatoFrontale = latoFrontale.map((cube) => cube.position.clone())
-    const initialPositionLatoPosteriore = latoPosteriore.map((cube) => cube.position.clone())
-    
-    setTimeout(() => {
-        latoDestro.forEach((cube, i) => {
-            cube.position.lerp(initialPositionLatoDestro[i], 0.1)
-        })
-        latoSinistro.forEach((cube, i) => {
-            cube.position.lerp(initialPositionLatoSinistro[i], 0.1)
-        })
-        latoSuperiore.forEach((cube, i) => {
-            cube.position.lerp(initialPositionLatoSuperiore[i], 0.1)
-        })
-        latoInferiore.forEach((cube, i) => {
-            cube.position.lerp(initialPositionLatoInferiore[i], 0.1)
-        })
-        latoFrontale.forEach((cube, i) => {
-            cube.position.lerp(initialPositionLatoFrontale[i], 0.1)
-        })
-        latoPosteriore.forEach((cube, i) => {
-            cube.position.lerp(initialPositionLatoPosteriore[i], 0.1)
-        })
-    })
-
-    const light = new THREE.PointLight(0xff0000, 1_000_000, 0, 0.01)
-    light.position.set(0, 0, 0).normalize()
-    group.add(light)
+    const positionLight = new THREE.PointLight(0xffffff, 1_000_000, 0, 0.01)
+    positionLight.position.set(0,0,0)
+    group.add(positionLight)
 
     scene.add(group)
 
-    // const _animatePosition = (cube: THREE.Mesh, prop: 'x' | 'y' | 'z', negative: boolean = false) => {
-    //     const targetPosition = cube.position.clone();
-    //     if (negative) {
-    //         targetPosition[prop] = -MAIN_CUBE_HALF * 3 - 1.5
-    //     } else {
-    //         targetPosition[prop] = MAIN_CUBE_HALF * 3 - 1.5
-    //     }
-    //     cube.position.lerp(targetPosition, 0.1)
-    // }
-    
-    const animateCubes = () => {
-
-        // latoDestro.forEach((cube) => {
-        //     _animatePosition(cube, 'x')
-        // })
-        // latoSinistro.forEach((cube) => {
-        //     _animatePosition(cube, 'x', true)
-        // })
-        // latoSuperiore.forEach((cube) => {
-        //     _animatePosition(cube, 'y')
-        // })
-        // latoInferiore.forEach((cube) => {
-        //     _animatePosition(cube, 'y', true)
-        // })
-        // latoFrontale.forEach((cube) => {
-        //     _animatePosition(cube, 'z')
-        // })
-        // latoPosteriore.forEach((cube) => {
-        //     _animatePosition(cube, 'z', true)
-        // })
-
-
-        // setTimeout(() => {
-        //     latoDestro.forEach((cube, i) => {
-        //         cube.position.lerp(initialPositionLatoDestro[i], 0.1)
-        //     })
-        //     latoSinistro.forEach((cube, i) => {
-        //         cube.position.lerp(initialPositionLatoSinistro[i], 0.1)
-        //     })
-        //     latoSuperiore.forEach((cube, i) => {
-        //         cube.position.lerp(initialPositionLatoSuperiore[i], 0.1)
-        //     })
-        //     latoInferiore.forEach((cube, i) => {
-        //         cube.position.lerp(initialPositionLatoInferiore[i], 0.1)
-        //     })
-        //     latoFrontale.forEach((cube, i) => {
-        //         cube.position.lerp(initialPositionLatoFrontale[i], 0.1)
-        //     })
-        //     latoPosteriore.forEach((cube, i) => {
-        //         cube.position.lerp(initialPositionLatoPosteriore[i], 0.1)
-        //     })
-        // }, 2000)
-        
-    }
-
-    return {mainCube, group, animateCubes}
+    return {group, latoDestro, latoSinistro, latoSuperiore, latoInferiore, latoFrontale, latoPosteriore, ...animateScene(latoDestro, latoSinistro, latoSuperiore, latoInferiore, latoFrontale, latoPosteriore)}
 }
 
 export const getScrollbarWidth = () => {
@@ -231,17 +186,18 @@ export const Scene = () => {
         )
         
         camera.position.z = 4
-        camera.position.x = 0
+        camera.position.x = -25
 
         const renderer = new THREE.WebGLRenderer({canvas: ref.current as HTMLCanvasElement})
         renderer.setSize(WIDTH, HEIGHT)
 
         new OrbitControls(camera, renderer.domElement)
 
-        const {group, animateCubes} = 
-            composeScene(scene)
+        const { group, latoDestro, latoSinistro, latoSuperiore, latoInferiore, latoFrontale, latoPosteriore } 
+            = composeScene(scene)
     
         window.addEventListener('resize', onWindowResize, false)
+
         function onWindowResize() {
             console.log('onWindowResize')
             
@@ -250,20 +206,21 @@ export const Scene = () => {
             renderer.setSize(window.innerWidth - scrollbarWidth, window.innerHeight - scrollbarWidth)
             render()
         }
-        function animate() {
-            requestAnimationFrame(animate)
-        
+
+        function render() {
             group.rotation.x += 0.005
             group.rotation.y += 0.005
 
-            render()
-            // animateCubes()
-        }
-
-        function render() {
             renderer.render(scene, camera)
         }
-        
+
+        function animate() {
+            requestAnimationFrame(animate)
+
+            render()
+        }
+
+        animateScene(latoDestro, latoSinistro, latoSuperiore, latoInferiore, latoFrontale, latoPosteriore)
         animate()
     }, [])
 
